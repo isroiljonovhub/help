7b565967b7e4da4cb1906dc376ba39b5f0b4f052
Zubayr <Z>
2025-07-30 15:28:38 +0500

    add text

---
+++ js/js/new.js
@@ -0,0 +1,229 @@
+//4
+let s = prompt("sonni kirit")
+if(s==10){
+    console.log("togri son!")
+}else{
+    console.log("boshqa son krtdngz!");
+}
+
+//5
+let soz1 = prompt("ha yoki yuq")
+if(soz1 == "ha"){
+    console.log("ruxsat bor")
+}else{
+    console.log("ruxsat yuq!")
+}
+
+//6
+let numbr = prompt("raqam krtng")
+if(numbr >0&&numbr<100){
+    console.log("yaxshi diapozon");
+}else{
+    console.log("tashqarida diapazon!");
+}
+
+//7
+let wrd = prompt("soz krt")
+if(wrd.length>5){
+    console.log("uzo soz");
+}else{
+    console.log("qska soz!");
+}
+
+//8
+let n = prompt("s0n krting")
+if(n%2==0){
+    console.log("juft s0n!");
+}else{
+    console.log("toq sn!");
+}
+
+//9
+let x = prompt("raqam")
+x = Number(x)
+if(x===0){
+    console.log("nol krtdngz");
+}else{
+    console.log("nol emas");
+}
+
+//10
+let a = prompt("so'z")
+if(a=="salom"){
+    console.log("salom duniyo");
+}else{
+    console.log("boshq soz");
+}
+
+//11
+let num1 = prompt("1-son")
+let num2 = prompt("2-son")
+if(num1>num2){
+    console.log("1 katta");
+}else{
+    console.log("2 katta yoki teng");
+}
+
+//12
+let ism1 = prompt("ism")
+let yos = prompt("yosh")
+if(yos>18 && ism1=="Ali"){
+    console.log("xushkeldz Ali");
+}else{
+    console.log("kirsh mumkunmas");
+}
+
+//13
+let r = prompt("sn krtn")
+if(r<10||r>100){
+    console.log("tashqari");
+}else{
+    console.log("ichida");
+}
+
+//14
+let s1 = prompt("krtng")
+if(s1!=""){
+    console.log("krtild");
+}else{
+    console.log("nma krtmaddz!");
+}
+
+//15
+let a1 = prompt("son1")
+let b1 = prompt("son2")
+if(a1==b1){
+    console.log("teng!");
+}else{
+    console.log("emaz");
+}
+
+//16
+let nm = prompt("sn")
+nm = Number(nm)
+if(nm>=50 && nm%2==0){
+    console.log("katta juft sn");
+}else{
+    console.log("boshqa");
+}
+
+//17
+let w = prompt("soz kiritt")
+if(w=="javascript"||w=="coding"){
+    console.log("siz dastuchsiz");
+}else{
+    console.log("boshq so'z");
+}
+
+//18
+let num0 = prompt("sn")
+if(num0!=0){
+    console.log("nol emass!");
+}else{
+    console.log("son nol");
+}
+
+//19
+let y = prompt("yosh")
+if(y>13 && y<19){
+    console.log("siz osmirrsz");
+}else{
+    console.log("osmr emasz");
+}
+
+//20
+let ss = prompt("krting soz")
+if(ss.length<3 || ss.length==0){
+    console.log("judaa qisqa soz");
+}else{
+    console.log("yaxsh soz");
+}
+
+//21
+let n1 = prompt("n1")
+let n2 = prompt("n2")
+if(n1>n2 && n1%2==0){
+    console.log("birni juft va katta");
+}else{
+    console.log("yoq");
+}
+
+//22
+let j = prompt("yoshn krtn")
+let jn = prompt("jnsni krtn")
+if(j>18 && jn=="erkak"){
+    console.log("armiyaga");
+}else{
+    console.log("bormsz");
+}
+
+//23
+let nm1 = prompt("s")
+if(nm1<10 || nm1>100 && nm1%2==1){
+    console.log("toq va chgarada");
+}else{
+    console.log("yoq");
+}
+
+//24
+let adm = prompt("adminmi")
+if(adm!="admin"){
+    console.log("emassiz!");
+}else{
+    console.log("xushkelbsz admin");
+}
+
+//25
+let a2 = prompt("1-son")
+let b2 = prompt("2-son")
+if(a2>=b2 && a2>=0){
+    console.log("mos keladi!");
+}else{
+    console.log("emas");
+}
+
+//26
+let sn1 = prompt("raqaam")
+sn1 = Number(sn1)
+if((sn1>0&&sn1<10)||(sn1>100&&sn1<200)){
+    console.log("maxsus dia");
+}else{
+    console.log("oddy son");
+}
+
+//27
+let i1 = prompt("ismm")
+let y1 = prompt("yosh")
+if(i1!="" && y1<18){
+    console.log("yoshsn "+i1);
+}else{
+    console.log("boshqa holat!");
+}
+
+//28
+let s8 = prompt("raqam krtng")
+s8 = Number(s8)
+if((s8!=0 && s8%2==0)||s8>100){
+    console.log("maxss son!");
+}else{
+    console.log("oddyy son");
+}
+
+//29
+let w1 = prompt("1-so'z")
+let w2 = prompt("2-so'z")
+if(w1.length>w2.length || w1=="salom"){
+    console.log("g'olb 1-");
+}else{
+    console.log("2-g'olib");
+}
+
+//30
+let y0 = prompt("yoshn krtn")
+let i0 = prompt("ism")
+let jv = prompt("ha yoki yuq")
+if(y0>18 && i0!="" && jv=="ha"){
+    console.log("sperstar "+i0);
+}else{
+    console.log("emas!");
+}
